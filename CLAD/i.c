3fb0c3f511eccdb0b16801fbc615fee879434332
Eeshwar <eesrut1@gmail.com>
2024-03-06 20:10:05 +0530

    Initial commit

---
+++ CLAD/i.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    inti = 10;
+    i = !i > 14;
+    printf("i=%d", i);
+}
---
+++ CLAD/r.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+printf("%d",scanf("%d”, i)); // value 10 is given as input here
+}
