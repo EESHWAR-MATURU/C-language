f8f4fa734c164af604b93a99fb7a80eda5e68e58
Eeshwar <eesrut1@gmail.com>
2024-03-11 18:24:53 +0530

    initial commit

---
+++ CLAD/y.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int a = 0,s = 0;
+while (s <= 10) // Statement-1
+{
+    a = a + 7 ;        // Statement-2
+    printf("%d",a);        // Statement-3
+    s = s + 1; // Statement-4
+}
+}
